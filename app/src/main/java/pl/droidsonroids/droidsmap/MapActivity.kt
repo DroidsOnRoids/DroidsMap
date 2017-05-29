3ed116556a6cfcf40db259c196f90198c6d08cdb
Haydart <makowiecki.radoslaw@gmail.com>
2017-05-29 18:51:11 +0200

    Initial commit

---
+++ app/src/main/java/pl/droidsonroids/droidsmap/MapActivity.kt
@@ -0,0 +1,11 @@
+package pl.droidsonroids.droidsmap
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+}
