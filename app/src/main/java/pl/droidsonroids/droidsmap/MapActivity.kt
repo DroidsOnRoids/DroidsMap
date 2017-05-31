5c99f878beae8ba7665a571d1e997698ae1c41b1
Haydart <makowiecki.radoslaw@gmail.com>
2017-05-31 18:06:56 +0200

    Hardcode base office mock with scene transitions

--- app/src/main/java/pl/droidsonroids/droidsmap/MapActivity.kt
+++ app/src/main/java/pl/droidsonroids/droidsmap/MapActivity.kt
@@ -2,10 +2,60 @@ package pl.droidsonroids.droidsmap
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.transition.Scene
+import android.transition.TransitionInflater
+import android.transition.TransitionManager
+import android.widget.GridLayout
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_map.*
+import kotlinx.android.synthetic.main.scene_office.*
+
 
 class MapActivity : AppCompatActivity() {
+    private var shouldMoveBack = true
+    private var officeScene: Scene? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        officeScene = Scene(rootLayout, officeSceneLayout)
+
+        roomImagesGridLayout.getRoomImages()
+                .forEach {
+                    it?.setOnClickListener {
+                        it?.transitionName = "room_transition"
+                        performRoomTransition()
+                    }
+                }
+    }
+
+    fun performRoomTransition() {
+        shouldMoveBack = false
+        val roomScene = Scene.getSceneForLayout(rootLayout, R.layout.scene_room, this)
+        val sceneTransition = TransitionInflater.from(this).inflateTransition(R.transition.room_scene_transition)
+        TransitionManager.go(roomScene, sceneTransition)
+    }
+
+    private fun performOfficeTransition() {
+        val sceneTransition = TransitionInflater.from(this).inflateTransition(R.transition.room_scene_transition)
+        TransitionManager.go(officeScene, sceneTransition)
+
+        roomImagesGridLayout.getRoomImages()
+                .forEach {
+                    it?.transitionName = ""
+                }
+    }
+
+    override fun onBackPressed() {
+        if (shouldMoveBack) {
+            super.onBackPressed()
+        } else {
+            performOfficeTransition()
+            shouldMoveBack = true
+        }
     }
 }
+
+private fun GridLayout.getRoomImages() = (0..childCount).map {
+    getChildAt(it) as ImageView?
+}.toList()
