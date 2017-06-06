e034e765d181bb8323c8f6d71edba3b9d0241484
Haydart <makowiecki.radoslaw@gmail.com>
2017-06-06 15:43:33 +0200

    Switch to Google Map implementation approach

--- app/src/main/java/pl/droidsonroids/droidsmap/MapActivity.kt
+++ app/src/main/java/pl/droidsonroids/droidsmap/MapActivity.kt
@@ -1,34 +1,132 @@
 package pl.droidsonroids.droidsmap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.transition.Scene
 import android.transition.Transition
 import android.transition.TransitionInflater
 import android.transition.TransitionManager
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
 import kotlinx.android.synthetic.main.activity_map.*
-import kotlinx.android.synthetic.main.scene_office.*
+import java.util.*
 
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnGroundOverlayClickListener {
+
+    private val droidsOnRoidsLocation: LatLng = LatLng(51.10944382158668, 17.0255388552323)
+    private val CAMERA_TRANSITION_DURATION_MILLIS = 500
+    private lateinit var map: GoogleMap
+
+    private lateinit var officeScene: Scene
+    private lateinit var currentRoom: ImageView
+    private val groundOverlayList = ArrayList<GroundOverlay>()
+    val MAP_BEARING = 201.5f
     private var shouldMoveBack = true
-    private var officeScene: Scene? = null
-    private var currentRoom: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+
+        val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         officeScene = Scene(rootLayout, officeSceneLayout)
 
-        roomsMap.getRoomImages()
+        /*roomsMap.getRoomImages()
                 .forEach {
                     it.setOnClickListener {
                         currentRoom = it as ImageView
                         it.transitionName = "room_transition"
                         performRoomTransition()
                     }
-                }
+                }*/
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        googleMap.setMinZoomPreference(18f)
+        map.isBuildingsEnabled = true
+        map.setOnGroundOverlayClickListener(this)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            map.isMyLocationEnabled = true
+        } else {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 200)
+            }
+        }
+
+        val bounds = LatLngBounds.builder()
+                .include(LatLng(droidsOnRoidsLocation.latitude - 0.0005, droidsOnRoidsLocation.longitude - 0.0005))
+                .include(LatLng(droidsOnRoidsLocation.latitude + 0.0005, droidsOnRoidsLocation.longitude + 0.0005))
+                .build()
+
+        map.setLatLngBoundsForCameraTarget(bounds)
+        map.uiSettings.isZoomControlsEnabled = false
+        map.uiSettings.isCompassEnabled = false
+        map.uiSettings.isMyLocationButtonEnabled = false
+
+        val cameraPosition = CameraPosition.Builder()
+                .bearing(MAP_BEARING)
+                .target(droidsOnRoidsLocation)
+                .zoom(20f)
+                .build()
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        map.moveCamera(cameraUpdate)
+
+        val newarkMap = GroundOverlayOptions()
+                .image(createMarkerBitmapDescriptor())
+                .transparency(0f)
+                .bearing(MAP_BEARING)
+                .position(droidsOnRoidsLocation, 6.90f, 4.30f)
+                .clickable(true)
+
+        val overlay = map.addGroundOverlay(newarkMap)
+        overlay.tag = "skybuds_room"
+        groundOverlayList.add(overlay)
+
+        map.addMarker(MarkerOptions().position(droidsOnRoidsLocation).title("Droids on Roids marker"))
+        map.moveCamera(CameraUpdateFactory.newLatLng(droidsOnRoidsLocation))
+    }
+
+    private fun createMarkerBitmapDescriptor(): BitmapDescriptor {
+        val vectorDrawable = ContextCompat.getDrawable(this, R.drawable.room_3)
+        val width = vectorDrawable.intrinsicWidth
+        val height = vectorDrawable.intrinsicHeight
+        vectorDrawable.setBounds(0, 0, width, height)
+        val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        vectorDrawable.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
+    }
+
+    override fun onGroundOverlayClick(groundOverlay: GroundOverlay) {
+        if (groundOverlay.tag == "skybuds_room") {
+            Toast.makeText(this, "Skybuds room overlay clicked!", Toast.LENGTH_SHORT).show()
+
+            val cameraPosition = CameraPosition.Builder()
+                    .bearing(MAP_BEARING)
+                    .target(groundOverlay.position)
+                    .zoom(25f)
+                    .build()
+
+            val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+            map.animateCamera(cameraUpdate, CAMERA_TRANSITION_DURATION_MILLIS, null)
+        }
     }
 
     fun performRoomTransition() {
@@ -49,16 +147,28 @@ class MapActivity : AppCompatActivity() {
         if (shouldMoveBack) {
             super.onBackPressed()
         } else {
-            performOfficeTransition()
+//            performOfficeTransition()
             shouldMoveBack = true
         }
     }
-}
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            200 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    map.isMyLocationEnabled = true
+                } else {
+                    Toast.makeText(this, "Grant the permission!", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+}
+/*
 private fun ConstraintLayout.getRoomImages() = (0..childCount)
         .map { getChildAt(it) }
         .filter { it is ImageView }
-        .toList()
+        .toList()*/
 
 private class TransitionListenerAdapter(private val endAction: () -> Unit) : Transition.TransitionListener {
 
